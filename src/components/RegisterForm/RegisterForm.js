25d4de6acd6de24c1ec167f7bc084cbca8d45193
AlexSidol <asidolaka@gmail.com>
2023-01-27 12:14:44 +0200

    add style

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -22,15 +22,15 @@ export const RegisterForm = () => {
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
       <label className={css.label}>
         Username
-        <input type="text" name="name" />
+        <input className={css.form__input} type="text" name="name" />
       </label>
       <label className={css.label}>
         Email
-        <input type="email" name="email" />
+        <input className={css.form__input} type="email" name="email" />
       </label>
       <label className={css.label}>
         Password
-        <input type="password" name="password" />
+        <input className={css.form__input} type="password" name="password" />
       </label>
       <button className={css.login__button} type="submit">
         Register
